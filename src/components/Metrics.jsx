d0b5bf2812fae1c9fbd274d88b1a913096e6ef74
Armaan Rasheed <arasheed@uwaterloo.ca>
2023-08-24 05:22:01 -0400

    fourth source change

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -68,7 +68,7 @@ export default function Metrics() {
                 <h1 className="title" id="metrics-title">
                     100,000
                 </h1>
-                <img src="./public/images/two-people.png" id="people-icon" alt="People Icon" />
+                <img src="./public/images/usa.png" id="people-icon" alt="People Icon" />
             </div>
             <div className="title" id="participants-title">
                 Participants
